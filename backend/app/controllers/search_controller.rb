35d8f6e93ff8987eb9705fe584b4092e3ff36915
João Carmo <kingjoao@gmail.com>
2022-04-15 16:15:27 +0100

    Move logic to model

--- backend/app/controllers/search_controller.rb
+++ backend/app/controllers/search_controller.rb
@@ -9,12 +9,7 @@ class SearchController < ApplicationController
       ingredients = query.split(',').map(&:strip)
     end
 
-    # Find recipes that match the ingredients
-    ingredients_with_wildcards = ingredients.map { |ingredient| "%#{ingredient}%" }
-    sql_query = Array.new(ingredients_with_wildcards.length) { "name ILIKE ?" }.join(' OR ')
-    found_ingredients = Ingredient.where(sql_query, *ingredients_with_wildcards)
-    @recipe_ingredients = RecipeIngredient.where(ingredient_id: found_ingredients.map(&:id))
-    @recipes = Recipe.where(id: @recipe_ingredients.map(&:recipe_id)).order(:title).page params[:page]
+    @recipes = Recipe.search_by_ingredients(ingredients).order(:title).page params[:page]
     count = Recipe.count
 
     serialized_recipes = ActiveModel::Serializer::CollectionSerializer.new(@recipes, each_serializer: RecipeSerializer)
--- backend/app/models/recipe.rb
+++ backend/app/models/recipe.rb
@@ -1,3 +1,13 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients
+
+  def self.search_by_ingredients(ingredients)
+    # Find recipes that match all the ingredients
+    ingredients_with_wildcards = ingredients.map { |ingredient| "%#{ingredient}%" }
+    sql_query = Array.new(ingredients_with_wildcards.length) { "name ILIKE ?" }.join(' OR ')
+    found_ingredients = Ingredient.where(sql_query, *ingredients_with_wildcards)
+    recipe_ingredients = RecipeIngredient.where(ingredient_id: found_ingredients.map(&:id))
+    recipe_ingredients_ids = recipe_ingredients.map(&:recipe_id).uniq
+    Recipe.where(id: recipe_ingredients_ids)
+  end
 end
