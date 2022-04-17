3403cf62a7d1867fb58a407fc480fd6fdffd60bb
João Carmo <kingjoao@gmail.com>
2022-04-17 11:23:48 +0100

    Database from URL

--- build_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker buildx build --platform linux/amd64 -f backend/production.Dockerfile -t registry.heroku.com/mydinnertime/backend backend/ && docker push registry.heroku.com/mydinnertime/backend &
-
-docker buildx build --platform linux/amd64 -f frontend/Dockerfile -t registry.heroku.com/mydinnertime/frontend frontend/ && docker push registry.heroku.com/mydinnertime/frontend &
-
-wait
