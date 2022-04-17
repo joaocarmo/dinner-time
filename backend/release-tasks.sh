d6384c2b19499acd7822fb2b1b2fd32849871145
João Carmo <kingjoao@gmail.com>
2022-04-17 12:25:20 +0100

    Add release tasks

---
+++ backend/release-tasks.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+## Step to execute
+bundle exec rails db:migrate
+
+# check for a good exit
+if [ $? -ne 0 ]
+then
+  # something went wrong; convey that and exit
+  exit 1
+fi
+
+# other code, potentially
